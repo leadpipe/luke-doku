d8847bedd5d97edfa2eb5326624bee23879edd36
Luke Blanshard <luke@blanshard.us>
2024-10-06 09:53:37 -0500

    Adds an icon button component.

---
+++ www/src/app/icon-button.ts
@@ -0,0 +1,52 @@
+import './mat-icon';
+import {css, html, LitElement} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+
+@customElement('icon-button')
+export class IconButton extends LitElement {
+  static override styles = css`
+    :host {
+      display: inline flex;
+      flex-direction: column;
+      font-size: x-small;
+      align-items: center;
+    }
+
+    button {
+      text-decoration: none;
+      cursor: pointer;
+      user-select: none;
+      -webkit-user-select: none;
+      color: inherit;
+      background: none;
+      border: none;
+      padding: 0;
+    }
+
+    button:disabled {
+      cursor: auto;
+      color: gray;
+    }
+  `;
+
+  override render() {
+    return html`
+      <button ?disabled=${this.disabled} title=${this.title}>
+        <mat-icon name=${this.iconName} size=${this.iconSize}></mat-icon>
+      </button>
+      ${this.label}
+    `;
+  }
+
+  @property() iconName: string = '';
+  @property() iconSize: string = '';
+  @property({type: Boolean}) disabled = false;
+  @property() override title: string = '';
+  @property() label: string = '';
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'icon-button': IconButton;
+  }
+}
