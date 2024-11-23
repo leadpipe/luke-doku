7c5fb52e87e3954502dbafe70ce915d4b61cfc96
Luke Blanshard <luke@blanshard.us>
2024-11-22 20:03:46 -0600

    Adds a way to ensure a switch statement covers all cases

--- www/src/app/utils.ts
+++ www/src/app/utils.ts
@@ -15,3 +15,13 @@ export function setBooleanAttribute(
     element.removeAttribute(attrName);
   }
 }
+
+/**
+ * Gets the compiler to ensure that a value being switched on (or tested using
+ * if statements) has had all possible values eliminated.  So if you change your
+ * code to allow another value, your call to this function will stop compiling.
+ * @param value The value being exhaustively switched on.
+ */
+export function ensureExhaustiveSwitch(value: never): never {
+  throw new Error(value);
+}
