21fa6f4cb048ea08e39d6f8577cb06c8ff20a16e
Luke Blanshard <luke@blanshard.us>
2024-11-28 20:56:22 -0600

    Reworks game history serialization to produce a byte array.

--- www/src/game/fake-data.ts
+++ www/src/game/fake-data.ts
@@ -20,13 +20,15 @@ import {
 } from './commands';
 import {Loc} from './loc';
 
+const FAKE_TIMESTAMP = (-1 >>> 0) + 1; // Doesn't fit in 32 bits
+
 export const FAKE_HISTORY: RecordedCommand[] = [
-  {command: new Resume(123), elapsedTimestamp: 0},
+  {command: new Resume(FAKE_TIMESTAMP), elapsedTimestamp: 0},
   {command: new SetNum(Loc.of(0), 1), elapsedTimestamp: 1000},
   {command: new SetNums(Loc.of(1), new Set([4, 2, 3])), elapsedTimestamp: 2000},
   {command: new Undo(), elapsedTimestamp: 3000},
   {command: new Pause(), elapsedTimestamp: 3001},
-  {command: new Resume(10123), elapsedTimestamp: 3001},
+  {command: new Resume(FAKE_TIMESTAMP + 10000), elapsedTimestamp: 3001},
   {command: new Redo(), elapsedTimestamp: 4000},
   {command: new UndoToStart(), elapsedTimestamp: 5000},
   {command: new RedoToEnd(), elapsedTimestamp: 6000},
@@ -45,27 +47,31 @@ export const FAKE_HISTORY: RecordedCommand[] = [
 ];
 
 // prettier-ignore
-export const FAKE_HISTORY_SERIALIZED: number[] = [
-  0,                // version
-  0, 123, 0,        // resume
-  5, 0, 1, 1000,    // setNum
-  6, 1, 423, 1000,  // setNums
-  7, 1000,          // undo
-  1, 1,             // pause
-  0, 10123, 0,      // resume
-  8, 999,           // redo
-  9, 1000,          // undoToStart
-  10, 1000,         // redoToEnd
-  4, 0, 1000,       // clearCell
-  11, 1000,         // createTrail
-  5, 1, 4, 1000,    // setNum
-  11, 1000,         // createTrail
-  16, 0, 0,         // copyFromTrail
-  12, 0, 1000,      // activateTrail
-  13, 1, 1000,      // toggleTrailVisibility
-  14, 1, 1000,      // archiveTrail
-  15, 1000,         // toggleTrailsActive
-  16, 1, 1000,      // copyFromTrail
-  2, 1, 1000,       // markCompleted (QUIT)
-  3, 2, 0,          // guessSolutionCount (2)
-];
+export const FAKE_HISTORY_SERIALIZED: Int8Array = new Int8Array([
+  0,                   // version
+  0, 
+    -128,-128,-128,-128,16,          // FAKE_TIMESTAMP
+          0,           // resume
+  5, 0, 1, -24,7,      // setNum     -- -24,7 === 1000
+  6, 1, -89,3, -24,7,  // setNums    -- -89,3 === 423
+  7, -24,7,            // undo
+  1, 1,                // pause
+  0, 
+    -112,-50,-128,-128,16,           // FAKE_TIMESTAMP + 10000
+          0,           // resume
+  8, -25,7,            // redo       -- -25,7 === 999
+  9, -24,7,            // undoToStart
+  10, -24,7,           // redoToEnd
+  4, 0, -24,7,         // clearCell
+  11, -24,7,           // createTrail
+  5, 1, 4, -24,7,      // setNum
+  11, -24,7,           // createTrail
+  16, 0, 0,            // copyFromTrail
+  12, 0, -24,7,        // activateTrail
+  13, 1, -24,7,        // toggleTrailVisibility
+  14, 1, -24,7,        // archiveTrail
+  15, -24,7,           // toggleTrailsActive
+  16, 1, -24,7,        // copyFromTrail
+  2, 1, -24,7,         // markCompleted (QUIT)
+  3, 2, 0,             // guessSolutionCount (2)
+]);
--- www/src/game/serialize.ts
+++ www/src/game/serialize.ts
@@ -28,13 +28,13 @@ import {Loc} from './loc';
 const VERSION = 0;
 
 /**
- * Turns a list of recorded commands into a list of numbers, which can later be
+ * Turns a list of recorded commands into a list of bytes, which can later be
  * converted back into the commands using `deserializeCommands`.
  * @param history The commands applied to a given Luke-doku game.
  */
-export function serializeCommands(history: RecordedCommand[]): number[] {
-  const answer = [VERSION];
-  const sink = (n: number) => answer.push(n);
+export function serializeCommands(history: RecordedCommand[]): Int8Array {
+  const array: number[] = [VERSION];
+  const sink = (n: number) => writeBase128(n, array);
   let prevTimestamp = 0;
   for (const c of history) {
     const s = serializersByCtor.get(c.command.constructor);
@@ -52,7 +52,7 @@ export function serializeCommands(history: RecordedCommand[]): number[] {
     sink(delta);
     prevTimestamp = c.elapsedTimestamp;
   }
-  return answer;
+  return new Int8Array(array);
 }
 
 /**
@@ -61,7 +61,7 @@ export function serializeCommands(history: RecordedCommand[]): number[] {
  * `serializeCommands`.
  * @throws Error if the input is malformed
  */
-export function deserializeCommands(serialized: number[]): RecordedCommand[] {
+export function deserializeCommands(serialized: Int8Array): RecordedCommand[] {
   const answer = [];
   let index = 0;
   const source = () => serialized[index++];
@@ -72,15 +72,56 @@ export function deserializeCommands(serialized: number[]): RecordedCommand[] {
   }
   let prevTimestamp = 0;
   while (index < serialized.length) {
-    const tag = source();
+    const tag = readBase128(source);
     const s = serializersByTag[tag];
     if (!s) {
       throw new Error(`No serializer found for tag ${tag}`);
     }
     const command = new s.ctor(...s.deserializeArgs(source));
-    const elapsedTimestamp = prevTimestamp + source();
+    const elapsedTimestamp = prevTimestamp + readBase128(source);
     prevTimestamp = elapsedTimestamp;
     answer.push({command, elapsedTimestamp});
+  }
+  return answer;
+}
+
+// Largest 32-bit int:
+const UINT_MAX = -1 >>> 0;
+
+/**
+ * Writes the given non-negative integer in "base 128" to the given array.  This
+ * is the protocol buffer "varint" algorithm: we write numbers larger than 127
+ * in pieces, 7 bits at a time, starting from the low bits.
+ */
+function writeBase128(i: number, array: number[]) {
+  if (i < 0) throw new Error(`Can't serialize negative numbers: ${i}`);
+  while (i > UINT_MAX) {
+    array.push((i & 127) | 128);
+    i = Math.floor(i / 128);
+  }
+  do {
+    let byte = i & 127;
+    i >>>= 7;
+    if (i > 0) {
+      byte |= 128;
+    }
+    array.push(byte);
+  } while (i);
+  return;
+}
+
+/**
+ * Does the opposite of `writeBase128`: reads a series of bytes and reconstructs
+ * the integer that produced it.
+ */
+function readBase128(source: () => number): number {
+  let answer = 0;
+  let multiplier = 1;
+  while (true) {
+    const byte = source();
+    answer += multiplier * (byte & 127);
+    if (byte >= 0) break;
+    multiplier *= 128;
   }
   return answer;
 }
@@ -109,7 +150,7 @@ const serializeTrailId = (
 };
 const deserializeTrailId: (
   source: () => number,
-) => [trailId: number] = source => [source()];
+) => [trailId: number] = source => [readBase128(source)];
 
 const resume: Serializer<typeof Resume> = {
   tag: 0,
@@ -118,7 +159,7 @@ const resume: Serializer<typeof Resume> = {
     sink(command.timestamp);
   },
   deserializeArgs(source) {
-    return [source()];
+    return [readBase128(source)];
   },
 };
 const pause: Serializer<typeof Pause> = {
@@ -134,7 +175,7 @@ const markCompleted: Serializer<typeof MarkCompleted> = {
     sink(command.completionState);
   },
   deserializeArgs(source): [state: CompletionState] {
-    return [source()];
+    return [readBase128(source)];
   },
 };
 const guessSolutionCount: Serializer<typeof GuessSolutionCount> = {
@@ -144,7 +185,7 @@ const guessSolutionCount: Serializer<typeof GuessSolutionCount> = {
     sink(command.guess);
   },
   deserializeArgs(source) {
-    return [source()];
+    return [readBase128(source)];
   },
 };
 const clearCell: Serializer<typeof ClearCell> = {
@@ -154,7 +195,7 @@ const clearCell: Serializer<typeof ClearCell> = {
     sink(command.loc.index);
   },
   deserializeArgs(source): [loc: Loc] {
-    return [Loc.of(source())];
+    return [Loc.of(readBase128(source))];
   },
 };
 const setNum: Serializer<typeof SetNum> = {
@@ -165,7 +206,7 @@ const setNum: Serializer<typeof SetNum> = {
     sink(command.num);
   },
   deserializeArgs(source): [loc: Loc, num: number] {
-    return [Loc.of(source()), source()];
+    return [Loc.of(readBase128(source)), readBase128(source)];
   },
 };
 const setNums: Serializer<typeof SetNums> = {
@@ -176,7 +217,10 @@ const setNums: Serializer<typeof SetNums> = {
     sink(Number([...command.nums].map(String).join('')));
   },
   deserializeArgs(source): [loc: Loc, nums: Set<number>] {
-    return [Loc.of(source()), new Set(String(source()).split('').map(Number))];
+    return [
+      Loc.of(readBase128(source)),
+      new Set(String(readBase128(source)).split('').map(Number)),
+    ];
   },
 };
 const undo: Serializer<typeof Undo> = {
