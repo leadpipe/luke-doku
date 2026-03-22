5a2848e35e44e55f0caae38ce0ea27b4849ef173
Luke Blanshard <luke@blanshard.us>
2026-03-22 17:34:58 -0500

    A bunch of changes to History-based navigation.
    It's not perfect, but the History API is essentially non-deterministic.

--- www/src/app/nav.ts
+++ www/src/app/nav.ts
@@ -1,7 +1,7 @@
 // Browser navigation module.
 
 import {PuzzleId, type Sudoku} from '../game/sudoku';
-import {EventType, logEvent} from '../system/analytics';
+import {EventType, log, logEvent} from '../system/analytics';
 import {dispatchTypeSafeEvent} from './events';
 import {puzzleTitleFromId} from './utils';
 
@@ -40,7 +40,8 @@ export function getHashState(url?: string): HashState {
   const hashIndex = url.indexOf('#');
   const hash = hashIndex >= 0 ? url.substring(hashIndex + 1) : '';
   const hashUrl = new URL('http://example.com/' + hash);
-  const path = hashUrl.pathname.substring(1).split('/');
+  const pathname = hashUrl.pathname.substring(1);
+  const path = pathname ? pathname.split('/') : [];
   return {
     path,
     params: hashUrl.searchParams,
@@ -56,9 +57,17 @@ function getUrl(hashState: HashState): string {
   return baseUrl + '#' + hashPath + (search ? '?' + search : '');
 }
 
+/** The path component for entering a custom puzzle. */
+export const ENTER_PUZZLE_PATH = 'enter-puzzle';
+
+/** The set of URL parameters that produce separate history entries. */
+const PUSHED_PARAMS = new Set(['d']); // dialog
+
 /**
  * Returns all prefix URLs for the given hash state, starting from the base URL,
- * through all the path components, and finally including all the parameters.
+ * through all the path components, and finally including the parameters. The
+ * last URL is the full URL for the given hash state, and each preceding URL is
+ * a prefix of it.
  */
 function getPrefixUrls(hashState: HashState): string[] {
   const urls: string[] = [];
@@ -72,12 +81,30 @@ function getPrefixUrls(hashState: HashState): string[] {
     urls.push(getUrl(hashState));
   }
   const params = new URLSearchParams();
+  const pushedParams = new URLSearchParams();
   for (const [key, value] of hashState.params) {
-    params.append(key, value);
+    if (PUSHED_PARAMS.has(key)) {
+      pushedParams.append(key, value);
+    } else {
+      params.append(key, value);
+    }
+  }
+  if (params.size) {
+    urls.pop(); // remove the full path URL, since the params will be different
+    urls.push(getUrl({path, params}));
+  }
+  if (pushedParams.size) {
+    for (const [key, value] of pushedParams) {
+      params.append(key, value);
+    }
     urls.push(getUrl({path, params}));
   }
   return urls;
 }
+
+export const TEST_ONLY = {
+  getPrefixUrls,
+};
 
 /**
  * Replaces the history entry at the given index with the given URL, or pushes a
@@ -103,6 +130,9 @@ function pushOrReplace(
 function getDocumentTitle(hashState: HashState): string {
   const {path} = hashState;
   if (path.length) {
+    if (path[0] === ENTER_PUZZLE_PATH) {
+      return 'Enter a Puzzle';
+    }
     const id = PuzzleId.parse(path[0]);
     if (id) {
       const puzzleTitle = puzzleTitleFromId(id, /*assumeToday=*/ false);
@@ -120,50 +150,57 @@ function updateEntriesAndTitle(
   index: number,
   url: string,
 ) {
-  const pushed = pushOrReplace(entries, index, url);
-  if (pushed) {
-    window.history.pushState(null, '', url);
-  } else {
-    window.history.replaceState(null, '', url);
-  }
   const hashState = getHashState(url);
   document.title = getDocumentTitle(hashState);
+  const pushed = pushOrReplace(entries, index, url);
+  if (pushed) {
+    window.history.pushState({index}, '', url);
+    log('PUSH', index, url);
+  } else {
+    window.history.replaceState({index}, '', url);
+    log('REPLACE', index, url);
+  }
 }
 
 /**
  * Aligns the given history stack with the given hash state by pushing,
  * replacing, or going back as needed.
  */
-function alignHistoryStack(
+async function alignHistoryStack(
   stack: HistoryStack,
   hashState: HashState,
   sendEvent = true,
 ) {
-  const {path} = hashState;
-  const prefixUrls = getPrefixUrls({path, params: new URLSearchParams()});
+  const prefixUrls = getPrefixUrls(hashState);
+  log('Aligning history stack with hash state', hashState, prefixUrls, stack);
+
   // Find the first entry that differs from the implied history stack.
   let i = 0;
   for (; i < prefixUrls.length; ++i) {
     const url = prefixUrls[i];
     if (stack.entries[i]?.url !== url) {
+      // Note this handles a short stack
       break;
     }
   }
-  // Go back if needed, or replace if the very first entry differs.
-  const targetIndex = i - 1;
-  if (targetIndex < 0) {
-    if (stack.index > 0) {
-      window.history.go(-stack.index);
-      stack.index = 0;
-    }
-    updateEntriesAndTitle(stack.entries, 0, prefixUrls[0]);
-  } else if (stack.index !== targetIndex) {
-    window.history.go(targetIndex - stack.index);
-    stack.index = targetIndex;
+  // The general approach is to jump forward or back, replace the first
+  // differing entry, and then push the rest of the entries.  However, if the
+  // new stack is shorter, we may need to jump to the last identical entry so
+  // there is something to push.
+  if (
+    i === stack.entries.length ||
+    i === prefixUrls.length ||
+    (i > 0 && i < stack.entries.length && i < prefixUrls.length - 1)
+  ) {
+    --i;
   }
+
+  await go(i - stack.index);
+  stack.index = i;
+
   // If we need to push new entries, first trim the array.
-  if (i < prefixUrls.length) {
-    stack.entries = stack.entries.slice(0, stack.index + 1);
+  if (i + 1 < prefixUrls.length) {
+    stack.entries = stack.entries.slice(0, i + 1);
   }
   for (; i < prefixUrls.length; ++i) {
     const url = prefixUrls[i];
@@ -171,7 +208,7 @@ function alignHistoryStack(
     stack.index = i;
   }
   window.sessionStorage.setItem('historyStack', JSON.stringify(stack));
-  if (sendEvent) handleHashStateChange();
+  if (sendEvent) handleHashStateChange({index: stack.index});
 }
 
 const baseUrl = window.location.href.replace(/(#.*)?$/, '');
@@ -209,13 +246,54 @@ const historyStack: Promise<HistoryStack> = (async () => {
     ],
   };
   window.history.replaceState(null, '', baseUrl);
-  alignHistoryStack(stack, getHashState(fullUrl), false);
+  await alignHistoryStack(stack, getHashState(fullUrl), false);
   return stack;
 })();
 
-window.addEventListener('popstate', handleHashStateChange);
+let popstateResolver: ((value: void | PromiseLike<void>) => void) | null = null;
 
-async function handleHashStateChange() {
+window.addEventListener('popstate', async event => {
+  if (popstateResolver) {
+    popstateResolver();
+    popstateResolver = null;
+    log('POPSTATE (awaited)', window.location.hash);
+  } else {
+    log('POPSTATE', window.location.hash);
+    await handleHashStateChange(event.state || {});
+  }
+});
+
+/**
+ * Goes forward or backward in the history stack, waiting until the popstate
+ * event is handled (or too much time has elapsed).
+ */
+async function go(delta: number): Promise<void> {
+  if (delta === 0) {
+    log('GO', delta, '(no-op)');
+    return;
+  }
+  const o = Promise.withResolvers<void>();
+  popstateResolver = o.resolve;
+  window.history.go(delta);
+  log('GO', delta);
+  await Promise.race([
+    o.promise,
+    new Promise<void>(resolve =>
+      setTimeout(() => {
+        if (popstateResolver === o.resolve) {
+          popstateResolver = null;
+          logEvent(EventType.ERROR, {
+            category: 'history.go timeout',
+            detail: `delta=${delta}`,
+          });
+        }
+        resolve();
+      }, 100),
+    ),
+  ]);
+}
+
+async function handleHashStateChange({index: stateIndex}: {index?: number}) {
   const stack = await historyStack;
   const fullUrl = window.location.href;
   const hashState = getHashState(fullUrl);
@@ -225,11 +303,24 @@ async function handleHashStateChange() {
     window.sessionStorage.setItem('historyStack', JSON.stringify(stack));
   } else {
     // The current URL is not in our history stack; rebuild the stack.
-    logEvent(EventType.ERROR, {
-      category: 'history stack missing entry',
-      detail: fullUrl,
+    if (stateIndex != null) {
+      logEvent(EventType.ACTION, {
+        category: 'user changed address bar',
+        detail: `${fullUrl} (${stateIndex})`,
+      });
+    } else {
+      logEvent(EventType.ERROR, {
+        category: 'history stack missing entry',
+        detail: fullUrl,
+      });
+    }
+    // Adjusts the stack to our best guess of the new history stack.
+    ++stack.index;
+    stack.entries.splice(stack.index, stack.entries.length - stack.index, {
+      index: stack.index,
+      url: fullUrl,
     });
-    alignHistoryStack(stack, hashState);
+    await alignHistoryStack(stack, hashState, false);
   }
   dispatchTypeSafeEvent(window, 'hash-state-changed', hashState);
 }
@@ -240,13 +331,6 @@ async function handleHashStateChange() {
  */
 export async function navigateHome() {
   return navigateToPath(); // empty path == home
-}
-
-/**
- * Navigates back one entry in history, if possible.
- */
-export async function navigateBack() {
-  window.history.back(); // Our popstate handler will update the stack.
 }
 
 /**
@@ -264,7 +348,7 @@ export async function navigateToPath(...path: string[]) {
  * prefixes of the path and parameters.
  */
 export async function navigateToHashState(hashState: HashState) {
-  alignHistoryStack(await historyStack, hashState);
+  await alignHistoryStack(await historyStack, hashState);
 }
 
 /**
@@ -280,7 +364,7 @@ export async function navigateToParam(key: string, value?: string) {
   } else {
     params.set(key, value);
   }
-  alignHistoryStack(stack, {path: currentHashState.path, params});
+  await alignHistoryStack(stack, {path: currentHashState.path, params});
 }
 
 /**
